d6facbd9d985a560f8f36f871dd608ca50c802c4
youthfulanger <prorock-666@inbox.ru>
2021-10-01 21:44:43 +0900

    another route fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="container">
       <header className="header">
-        <Link to="/">
+        <Link to={process.env.PUBLIC_URL + '/'}>
           <div className="header__text">MoodBoard</div>
         </Link>
 
